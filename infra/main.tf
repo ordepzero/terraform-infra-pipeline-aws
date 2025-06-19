125f8d38ad9f5509d54d3832fd798d19084c0efa
ordepzero <ulissesph@gmail.com>
2025-06-19 19:44:07 -0300

    Inclusão dos buckets da bosvespa

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,9 @@
 
 
-resource "aws_s3_bucket" "bucket_video" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "bucket_bovespa_raw" {
+  bucket = var.bucket_name_bovespa_raw
+}
+
+resource "aws_s3_bucket" "bucket_bovespa_refined" {
+  bucket = var.bucket_name_bovespa_refined
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,10 @@
-variable "bucket_name" {
+variable "bucket_name_bovespa_raw" {
+  description = "The name of the S3 bucket to create."
+  type        = string
+  
+}
+
+variable "bucket_name_bovespa_refined" {
   description = "The name of the S3 bucket to create."
   type        = string
   
