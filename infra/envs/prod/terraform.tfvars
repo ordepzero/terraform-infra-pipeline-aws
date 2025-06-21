0e3016f7c5f3a9ecaf27366139c5924927a6b294
ordepzero <ulissesph@gmail.com>
2025-06-21 10:24:29 -0300

    Generic resources

--- infra/envs/prod/terraform.tfvars
+++ infra/envs/prod/terraform.tfvars
@@ -1,2 +1,3 @@
+enviroment = "prod"
 bucket_name_bovespa_raw = "prod-sa-east-1-bovespa-raw"
 bucket_name_bovespa_refined = "prod-sa-east-1-bovespa-refined"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,7 @@
+variable "enviroment" {
+  description = "The environment for which the infrastructure is being provisioned. It can be 'dev', 'hom', or 'prod'."
+  type        = string
+}
 variable "bucket_name_bovespa_raw" {
   description = "The name of the S3 bucket to create bucket_name_bovespa_raw."
   type        = string
