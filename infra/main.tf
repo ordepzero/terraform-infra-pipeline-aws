0afe73981016600f525d91e9bcbe4384b5e7b673
ordepzero <ulissesph@gmail.com>
2025-06-21 08:22:27 -0300

    Criando usuário

--- infra/main.tf
+++ infra/main.tf
@@ -5,3 +5,64 @@ resource "aws_s3_bucket" "bucket_bovespa_raw" {
 resource "aws_s3_bucket" "bucket_bovespa_refined" {
   bucket = var.bucket_name_bovespa_refined
 }
+
+
+########################
+### CRIANDO UMA ROLE ###
+########################
+
+resource "aws_iam_user" "user" {
+  name = "test-user"
+}
+
+resource "aws_iam_role" "role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_group" "group" {
+  name = "test-group"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  users      = [aws_iam_user.user.name]
+  roles      = [aws_iam_role.role.name]
+  groups     = [aws_iam_group.group.name]
+  policy_arn = aws_iam_policy.policy.arn
+}
