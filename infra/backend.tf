7e125f46183bfb3fd542ca08ea35ed99e1cc974c
ordepzero <ulissesph@gmail.com>
2025-06-21 14:37:32 -0300

    FGGHH

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "ordepzero-sa-east-1-terraform-statefile"
+    region         = "sa-east-1"
+    dynamodb_table = "ordepzero-sa-east-1-terraform-lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "sa-east-1"
 }
