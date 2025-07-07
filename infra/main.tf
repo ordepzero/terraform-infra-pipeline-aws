282aa5dbfe8a86fda88d1fea8c10624876f137e3
ordepzero <ulissesph@gmail.com>
2025-07-06 23:37:18 -0300

    Zip 4

--- infra/main.tf
+++ infra/main.tf
@@ -36,7 +36,7 @@ resource "aws_s3_object" "glue_etl_script" {
 # Este data source cria um arquivo ZIP localmente.
 data "archive_file" "python_utils_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/app/utils" # Caminho local para o diretório de utilitários
+  source_dir  = "${path.module}/../app/utils/" # Caminho local para o diretório de utilitários
   output_path = "app/utils.zip" # Nome do arquivo ZIP que será criado localmente
   # Garanta que o diretório 'app/utils' exista localmente
 }
