3ea451e2025b46fb0ab85588b7cf42f51c9fb2cf
ordepzero <ulissesph@gmail.com>
2025-07-12 21:25:48 -0300

    Criação do database

---
+++ infra/modules/table/main.tf
@@ -0,0 +1,15 @@
+resource "aws_glue_catalog_database" "example" {
+  name = "MyCatalogDatabase"
+}
+
+resource "aws_glue_catalog_database" "example" {
+  name = "MyCatalogDatabase"
+
+  create_table_default_permission {
+    permissions = ["SELECT"]
+
+    principal {
+      data_lake_principal_identifier = "IAM_ALLOWED_PRINCIPALS"
+    }
+  }
+}
