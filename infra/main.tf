48811a614e234c4d96b062351ef719789736919c
ordepzero <ulissesph@gmail.com>
2025-07-27 22:29:29 -0300

    Ajuste endpoint

--- infra/main.tf
+++ infra/main.tf
@@ -134,16 +134,24 @@ data "aws_route_table" "glue_job_subnet_route_table" {
 }
 
 # 3. Referencia o S3 Gateway Endpoint existente.
-data "aws_vpc_endpoint" "s3_gateway_existing" {
+# Usamos a fonte de dados no plural para filtrar de forma confiável pelo tipo de endpoint.
+data "aws_vpc_endpoints" "s3_gateway_existing" {
   vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.region}.s3"
-  vpc_endpoint_type = "Gateway"
+  filter {
+    name   = "service-name"
+    values = ["com.amazonaws.${data.aws_region.current.region}.s3"]
+  }
+  filter {
+    name   = "vpc-endpoint-type"
+    values = ["Gateway"]
+  }
 }
 
 # 4. Garante que a tabela de rotas da sub-rede do Glue esteja associada ao S3 Gateway Endpoint.
 # Isso resolve o erro "Could not find S3 endpoint" que o Glue Job pode encontrar.
 resource "aws_vpc_endpoint_route_table_association" "s3_route_association" {
-  vpc_endpoint_id = data.aws_vpc_endpoint.s3_gateway_existing.id
+  # Pega o ID do primeiro (e único) endpoint Gateway encontrado.
+  vpc_endpoint_id = one(data.aws_vpc_endpoints.s3_gateway_existing.ids)
   route_table_id  = data.aws_route_table.glue_job_subnet_route_table.id
 }
 
